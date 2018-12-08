5600c9f684f84952c16ec368318d0d68434f4fd8
Wallinmedia <post@mariuswallin.no>
2018-12-08 17:10:04 +0100

    Basic DB setup

--- server/src/app.js
+++ server/src/app.js
@@ -2,19 +2,24 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
-
+const {
+	sequelize
+} = require("./models");
+const config = require("./config/config");
 const app = express();
+
+const routes = require("./routes");
+
+routes(app);
+
 
 app.use(morgan("combined"));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/register', (req, res) => {
-	res.send({
-		message: `Hello ${req.body.email}! Your user was registered`
-	});
-});
 
-app.listen(process.env.PORT || 8080, () => {
-	console.log(`Server started`);
-});
+sequelize.sync()
+	.then(() => {
+		app.listen(config.port);
+		console.log(`Server started on ${config.port}`);
+	});
